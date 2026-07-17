5e2cf66d20999682a652581c89167c22c165c9a0
Rohit Pawar <rohitpawar7058@gmail.com>
2026-07-17 22:28:25 +0530

    Today ill learn about the JSX

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,14 @@
-function App() {
-  return (
-    <div>
-      <h1>💰 Expense Tracker</h1>
+function App () {
 
-      <p>Welcome to my first React Project!</p>
-    </div>
+  const name = "Rohit";
+  return (
+    <>
+     <h1>Expense Tracker</h1>
+     {/* <h2>Welcome Rohit</h2> */}
+     <h1> Welcome {name} </h1>
+     <p> Learning React with ChatGPT</p>
+    </>
   );
 }
-
 export default App;
+// {} --> It means execute the JavaScript code inside the curly braces and return the calue of the expression. In this case, it will return the value of the variable name which is "Rohit". 
